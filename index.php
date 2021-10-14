11b1c6b3c87eebb3ce526160d7952a2dc382af7b
Dmitriy <stdimensiy@yandex.ru>
2021-10-14 23:21:35 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Привет мир!!! <br>';
+        echo 'Проект My Simple REST server стартовал (на PHP)<br>';
+        echo 'Это исправление я внес через IDE PHP Storm (idea)<br>';
+        echo 'А вот это уже через через IDE NetBeans';
+        ?>
+    </body>
+</html>
