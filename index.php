a2dadadb642c8ea6df9c48788c1a986ba7d782fe
Dmitriy <stdimensiy@yandex.ru>
2021-10-16 01:09:56 +0300

    Изм 1.1 - Сформирована заглушка ответа с минимальным набором данных, но уже формальный json ответ

--- index.php
+++ index.php
@@ -1,21 +1,58 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo 'Привет мир!!! <br>';
-        echo 'Проект My Simple REST server стартовал (на PHP)<br>';
-        echo 'Это исправление я внес через IDE PHP Storm (idea)<br>';
-        echo 'А вот это уже через через IDE NetBeans<br>';
-        echo 'Тестовое изменение в режиме отслеживания измеенений (инициализация Git)<br>';
-        ?>
-    </body>
-</html>
+<?php
+$httpVersion = "HTTP/1.1";
+$contentType = "application/json; charset=UTF-8";
+$statusCode = 200;
+$rawData = "Проект My Simple REST API server стартовал (на PHP)";
+$response = "";
+header($httpVersion. " ". $statusCode ." ". getHttpStatusMessage($statusCode));
+header("Content-Type:". $contentType);
+//$response = json_encode($rawData);
+$response = $rawData;
+echo $response;
+
+function getHttpStatusMessage($statusCode){
+	$httpStatus = array(
+		100 => 'Continue',
+		101 => 'Switching Protocols',
+		200 => 'OK!',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found',
+		303 => 'See Other',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		306 => '(Unused)',
+		307 => 'Temporary Redirect',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported');
+	return ($httpStatus[$statusCode]) ? $httpStatus[$statusCode] : $httpStatus[500];
+}
+?>
